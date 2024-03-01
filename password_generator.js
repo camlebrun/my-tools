7f8bd66ff63ac90f33ea8d1da449263bf92b9416
Camille Lebrun <116400782+camlebrun@users.noreply.github.com>
2024-03-01 22:32:54 +0100

    deploy

--- password_generator.js
+++ password_generator.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
-    $("#generate-btn").click(function() {
-      var length = $("#length").val();
-      var uppercase = $("#uppercase").prop("checked");
-      var lowercase = $("#lowercase").prop("checked");
-      var numbers = $("#numbers").prop("checked");
-      var symbols = $("#symbols").prop("checked");
-  
-      var password = generatePassword(length, uppercase, lowercase, numbers, symbols);
-  
-      $("#password").val(password);
-    });
-  
-    function generatePassword(length, uppercase, lowercase, numbers, symbols) {
-      var charset = "";
-      if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-      if (numbers) charset += "0123456789";
-      if (symbols) charset += "!@#$%^&*()-_+=";
-  
-      var password = "";
-      for (var i = 0; i < length; i++) {
-        password += charset.charAt(Math.floor(Math.random() * charset.length));
-      }
-      return password;
-    }
+  $("#generate-btn").click(function() {
+    var length = $("#length").val();
+    var uppercase = $("#uppercase").prop("checked");
+    var lowercase = $("#lowercase").prop("checked");
+    var numbers = $("#numbers").prop("checked");
+
+    var password = generatePassword(length, uppercase, lowercase, numbers);
+    $("#password").val(password);
   });
-  
+
+  function generatePassword(length, uppercase, lowercase, numbers) {
+    var charset = "";
+    if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+    if (numbers) charset += "0123456789";
+    
+    // Add more symbols excluding those you want to avoid
+    charset += "!@#$%^&*()-_+=";
+
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+  }
+});
