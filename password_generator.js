59d1b01965cb8ef865f5646aafc0389fdaee27e4
Camille Lebrun <116400782+camlebrun@users.noreply.github.com>
2024-03-05 08:13:40 +0100

    feat: :art: design

--- password_generator.js
+++ password_generator.js
@@ -1,27 +1,34 @@
-$(document).ready(function() {
-  $("#generate-btn").click(function() {
-    var length = $("#length").val();
-    var uppercase = $("#uppercase").prop("checked");
-    var lowercase = $("#lowercase").prop("checked");
-    var numbers = $("#numbers").prop("checked");
+// passwordGenerator.js
 
-    var password = generatePassword(length, uppercase, lowercase, numbers);
-    $("#password").val(password);
-  });
+function generatePassword() {
+  var minLength = 12;
+  var maxLength = 32;
 
-  function generatePassword(length, uppercase, lowercase, numbers) {
-    var charset = "";
-    if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-    if (numbers) charset += "0123456789";
-    
-    // Add more symbols excluding those you want to avoid
-    charset += "!@#$%^&*()-_+=";
-
-    var password = "";
-    for (var i = 0; i < length; i++) {
-      password += charset.charAt(Math.floor(Math.random() * charset.length));
-    }
-    return password;
+  var length = parseInt($("#length").val());
+  if (length < minLength || length > maxLength) {
+    alert("Password length must be between 12 and 32 characters.");
+    return;
   }
-});
+
+  var includeSymbols = $("#includeSymbols").is(":checked");
+  var includeNumbers = $("#includeNumbers").is(":checked");
+  var includeUppercase = $("#includeUppercase").is(":checked");
+
+  var charset = "abcdefghijklmnopqrstuvwxyz";
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeNumbers) charset += "0123456789";
+  if (includeSymbols) charset += "!@#$%^&*()_+{}[]<>";
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  $("#password").val(password);
+
+  // Dynamically adjust the width of the password input field based on password length
+  var passwordLength = password.length;
+  var inputWidth = 15 * passwordLength + 30; // Adjust this formula for desired width
+  $("#password").css("width", inputWidth + "px");
+}
